ca957d2f90ee4bcf947dcf4c263b9912b6a5b1f9
RahulGupta2005 <harsh9094709@gmail.com>
2026-05-30 21:51:13 +0530

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,18 @@
+from deep_translator import GoogleTranslator
+
+
+# Translate Text Function
+def translate_text(text, target_language):
+
+    try:
+
+        translated = GoogleTranslator(
+            source='auto',
+            target=target_language
+        ).translate(text)
+
+        return translated
+
+    except Exception as e:
+
+        return f"Translation Error: {e}"
